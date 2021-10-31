019bcd31b3940ffb4666bba5b3edcf387c2930f2
jia <jbia@itu.dk>
2021-10-31 19:55:56 +0100

    Message and timestamps work

---
+++ client/client.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	pb "github.com/birkj/Miniproject2_Jens/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:8080"
+)
+
+var client pb.ChittyChatClient
+var wait *sync.WaitGroup
+
+func init() {
+	wait = &sync.WaitGroup{}
+}
+
+func connect(user *pb.User) error {
+	var streamerror error
+	stream, err := client.CreateStream(context.Background(), &pb.Connect{
+		User:   user,
+		Active: true,
+	})
+	if err != nil {
+		return fmt.Errorf("connection has failed: %v", err)
+	}
+	log.Println("Connecting to server. Timestamp: ", user.Time)
+	wait.Add(1)
+	//Recieve
+	go func(str pb.ChittyChat_CreateStreamClient) {
+		defer wait.Done()
+		for {
+			msg, err := str.Recv()
+			if err != nil {
+				streamerror = fmt.Errorf("error reading message: %v", err)
+				break
+			}
+			sendingUserTime := msg.From.Time
+			currentClientTime := user.Time
+
+			user.Time = calcUserTimeLamport(sendingUserTime, currentClientTime)
+
+			log.Println("From: ", msg.From.Name, "Message:", msg.Message, "Timestamp:", user.Time)
+		}
+	}(stream)
+	return streamerror
+}
+
+func calcUserTimeLamport(incomming int32, cur int32) int32 {
+
+	if cur > incomming {
+		return cur + 1
+	} else {
+		return incomming + 1
+	}
+}
+
+func main() {
+	timestamp := time.Now()
+	done := make(chan int)
+
+	name := flag.String("Name", "Anon", "The name of the user")
+	flag.Parse()
+
+	id := sha256.Sum256([]byte(timestamp.String() + *name))
+
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("could not connect to service : %v", err)
+	}
+
+	client = pb.NewChittyChatClient(conn)
+	user := &pb.User{
+		Id:   hex.EncodeToString(id[:]),
+		Name: *name,
+		Time: 1,
+	}
+
+	connect(user)
+
+	wait.Add(1)
+	go func() {
+		defer wait.Done()
+
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			user.Time++
+			msg := &pb.ChatMessage{
+				Id:      user.Id,
+				From:    user,
+				Message: scanner.Text(),
+			}
+
+			log.Println("Sending message... Timestamp:", user.Time)
+
+			_, err := client.BroadcastMessage(context.Background(), msg)
+			if err != nil {
+				fmt.Printf("error sending message: %v", err)
+				break
+			}
+
+		}
+	}()
+
+	go func() {
+		wait.Wait()
+		close(done)
+	}()
+
+	<-done
+}
---
+++ server/server.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/birkj/Miniproject2_Jens/proto"
+	"google.golang.org/grpc"
+)
+
+const port = ":8080"
+
+type Connection struct { // Creating a struct to contain each connection from clients
+	stream pb.ChittyChat_CreateStreamServer // Each connection contains a stream object which is used to send the message during the broadcast function
+	id     string
+	name   string
+	active bool // A bool used to check if a connection is still active
+	error  chan error
+}
+
+type Server struct {
+	pb.UnimplementedChittyChatServer
+	Connection  []*Connection
+	logicaltime int32
+}
+
+func (s *Server) CreateStream(pconn *pb.Connect, stream pb.ChittyChat_CreateStreamServer) error { // Once a user wants to connect, we create a stream object and add their usercredentials to it in a connection stuct
+	conn := &Connection{
+		stream: stream,
+		active: true,
+		error:  make(chan error),
+	}
+
+	//s.logicaltime = calcServerTimeLamport(pconn.User.Time, s.logicaltime)
+	s.Connection = append(s.Connection, conn) // connection is added to the server
+
+	_, err := s.BroadcastMessage(context.Background(), &pb.ChatMessage{
+		Message: pconn.User.Name + " has connected.",
+		From:    pconn.User,
+	})
+	//log.Println("From:", msg.From.Name, "Message:", msg.Message, "Timestamp:", s.logicaltime)
+	if err != nil {
+		log.Printf("could send connection message: %v", err)
+	}
+
+	return <-conn.error // channel any errors out
+}
+
+func (s *Server) BroadcastMessage(ctx context.Context, msg *pb.ChatMessage) (*pb.Empty, error) {
+
+	cur := s.logicaltime
+	incomming := msg.From.Time
+	s.logicaltime = calcServerTimeLamport(cur, incomming)
+	log.Println("From:", msg.From.Name, "Message:", msg.Message, "Timestamp:", s.logicaltime)
+	s.logicaltime++
+
+	log.Println("Broadcasting... to all users. Timestamp:", s.logicaltime)
+	for _, conn := range s.Connection {
+
+		go func(msg *pb.ChatMessage, conn *Connection) {
+
+			if conn.active {
+				msg.From.Time = s.logicaltime
+				//fmt.Print(msg)
+				err := conn.stream.Send(msg)
+
+				if err != nil {
+					fmt.Println(err)
+				}
+			}
+		}(msg, conn)
+	}
+
+	return &pb.Empty{}, nil
+}
+
+// func (s *Server) CalculateTime(msg *pb.ChatMessage) []int32 {
+// 	var times []int32
+
+// 	times = append(times, s.logicaltime)
+
+// 	for _, conn := range s.Connection {
+// 		if msg.GetId() == conn.id {
+// 			conn.logicaltime += 1
+// 		}
+// 		times = append(times, conn.logicaltime)
+// 	}
+
+// 	return times
+// }
+
+func calcServerTimeLamport(cur int32, incomming int32) int32 {
+
+	if cur > incomming {
+		return cur + 1
+	} else {
+		return incomming + 1
+	}
+}
+
+func main() {
+	grpcServer := grpc.NewServer()
+	listener, err := net.Listen("tcp", port)
+
+	if err != nil {
+		log.Fatalf("Error, couldn't create the server %v", err)
+	}
+
+	server := Server{logicaltime: 1}
+	log.Println("Starting server at port", port, "Timestamp:", server.logicaltime)
+	pb.RegisterChittyChatServer(grpcServer, &server)
+	grpcServer.Serve(listener)
+
+}
